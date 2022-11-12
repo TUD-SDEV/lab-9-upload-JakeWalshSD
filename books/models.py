374354b474ab76b0deeabd2bebb039cbb644b66b
Jake Walsh <jakewalsh2019@gmail.com>
2022-11-12 21:00:10 +0000

    Lab 9 Part 5

--- books/models.py
+++ books/models.py
@@ -17,3 +17,4 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse('book_detail', args=[str(self.id)])
+
--- books/views.py
+++ books/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView, DetailView
 from .models import Book
-
+from django.db.models import Q
 # Create your views here.
 
 class BookListView(ListView):
@@ -11,3 +11,15 @@ class BookListView(ListView):
 class BookDetailView(DetailView):
  model = Book
  template_name = 'books/book_detail.html'
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+    #queryset = Book.objects.filter(title__icontains='Django') #new
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+      )
